a8b869b1b96d1caab1c496fff91927bfcfb22300
Ying <ying__li@hotmail.com>
2022-09-25 18:15:33 -0400

    changes

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(left, bottom, src) {
+    let images = document.createElement('img')
+    images.src = src
+    images.style.position = 'fixed'
+    images.style.left = left
+    images.style.bottom = bottom
+    document.body.append(images)
+
+    return images
+}
+
+newImage('450px', '200px', 'assets/pine-tree.png')
+newImage('100px', '100px', 'assets/green-character.gif')
+newImage('200px', '300px', 'assets/tree.png')
+newImage('350px', '100px', 'assets/pillar.png')
+newImage('150px', '200px', 'assets/crate.png')
+newImage('500px', '425px', 'assets/well.png')
+
+function newItem(left, bottom, src) {
+    let item = newImage(left, bottom, src)
+    item.addEventListener('dblclick', function(){
+        item.remove()
+    })
+}
+
+newItem('500px', '405px', 'assets/sword.png')
+newItem('165px', '185px', 'assets/sheild.png')
+newItem('600px', '100px', 'assets/staff.png')
